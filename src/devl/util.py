9da3e61fe3ecc7d7ab53915e4ab72294c8407101
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-03-21 10:02:21 +0100

    jenkins argument cli

--- src/devl/util.py
+++ src/devl/util.py
@@ -1,3 +1,4 @@
+import getopt
 import os
 import sys
 import jenkins
@@ -5,8 +6,40 @@ from dotenv import load_dotenv
 import jenkins_job_config
 
 
-def main(argv):
-    name = os.path.basename(os.getcwd())
+def main(args):
+    if len(args) > 0:
+        name = general_args(args[1:])
+        value = args[0]
+        if value in ("j", "jenkins"):
+            validate_args(name)
+            create_jenkins_job(name)
+        else:
+            raise Exception("What do you want to do?")
+    else:
+        raise Exception("What do you want to do?")
+
+
+def validate_args(*args):
+    for arg in args:
+        if arg is None:
+            raise Exception("Required arg missing.")
+
+
+def general_args(args):
+    name = None
+    opts, args = getopt.getopt(args, "n:", ["name="])
+    for opt, arg in opts:
+        if opt in ("-n", "--name"):
+            if arg == ".":
+                name = os.path.basename(os.getcwd())
+            else:
+                name = arg
+        else:
+            raise Exception(f"Unknown argument: {opt}")
+    return name
+
+
+def create_jenkins_job(name):
     server = jenkins.Jenkins('https://jenkins.wyss.tech', username='admin', password=os.getenv('jenkinsPassword'))
     server.create_job(name, jenkins_job_config.config.replace("{{name}}", name))
 
@@ -18,4 +51,3 @@ if __name__ == '__main__':
         main(sys.argv[1:])
     except Exception as e:
         print(e)
-
